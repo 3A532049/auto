475c077f8b5577b3b9f03c2801b4ee01f73df465
3A532049 <shelly213334@gmail.com>
2018-10-10 01:14:13 +0800

    public/index.php

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+require '../vendor/autoload.php';
+
+use \Demo\HelloWorld as World;
+use Demo\Hello\Lara;
+use Demo\Hello;
+
+$world = new World();
+$lara= new Lara();
+$vincent= new Hello\Someone('Vincent');
+
+// 以下Someone類別的使用可以不用use Demo\Hello;
+$mary= new \Demo\Hello\Someone('Mary');
+$john= new Demo\Hello\Someone('John');
+
+/**
+ * Created by PhpStorm.
+ * User: 游智萱
+ * Date: 2018/10/10
+ * Time: 上午 01:13
+ */
