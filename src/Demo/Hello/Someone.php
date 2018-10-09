b6306fc10c0e648dd47c179df5ea43fff8c94225
3A532049 <shelly213334@gmail.com>
2018-10-10 01:12:48 +0800

    ./src/Demo/Hello/Someone.php

---
+++ src/Demo/Hello/Someone.php
@@ -0,0 +1,17 @@
+<?php
+namespace Demo\Hello;
+
+class Someone
+{
+    function __construct($name)
+    {
+        echo 'Hello '.$name.PHP_EOL;
+    }
+}
+
+/**
+ * Created by PhpStorm.
+ * User: 游智萱
+ * Date: 2018/10/10
+ * Time: 上午 01:12
+ */
