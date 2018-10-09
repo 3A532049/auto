4b3204e37192a18ac2355dec966d38b62fb81f1b
3A532049 <shelly213334@gmail.com>
2018-10-10 01:11:40 +0800

    ./src/Demo/Hello/Lara.php

---
+++ src/Demo/Hello/Lara.php
@@ -0,0 +1,17 @@
+<?php
+namespace Demo\Hello;
+
+class Lara
+{
+    function __construct()
+    {
+        echo 'Hello Lara~'.PHP_EOL;
+    }
+}
+
+/**
+ * Created by PhpStorm.
+ * User: 游智萱
+ * Date: 2018/10/10
+ * Time: 上午 01:11
+ */
