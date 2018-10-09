2dc446cfce9f920c7a9909c2455750c5cef5431b
3A532049 <shelly213334@gmail.com>
2018-10-10 01:10:28 +0800

    src/Demo/src/Demo/HelloWorld.php

---
+++ src/Demo/HelloWorld.php
@@ -0,0 +1,17 @@
+<?php
+namespace Demo;
+
+class HelloWorld
+{
+    function __construct()
+    {
+        echo 'Hello World ~'.PHP_EOL;
+    }
+}
+
+/**
+ * Created by PhpStorm.
+ * User: 游智萱
+ * Date: 2018/10/10
+ * Time: 上午 01:09
+ */
